069be0f0c320a9588d34eeb79143fd02bd6e4f5b
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2021-04-28 15:23:21 +0200

    fix storageclass

--- tools/generate-gpg-secret.sh
+++
@@ -1,4 +0,0 @@
-gpg --export-secret-keys --armor "${FLUX_KEY_FP}" |
-kubectl create secret generic sops-gpg --dry-run=client -o yaml \
-    --namespace=flux-system \
-    --from-file=sops.asc=/dev/stdin >> gpg-secret.yaml
